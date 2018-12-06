0578eccbd242586bfd63259d4f9a5682673ccc3f
Frank811 <45404050+Frank811@users.noreply.github.com>
2018-12-05 20:49:43 -0600

    Add files via upload

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,37 +1,44 @@
 import fresh_tomatoes
 import media
 
-lego_movie = media.Movie("Lego Movie",
-                        "Legos come to life",
-                        "https://upload.wikimedia.org/wikipedia/en/1/10/The_Lego_Movie_poster.jpg",
-                        "https://www.youtube.com/watch?v=fZ_JOBCLF-I")
-#print(toy_story.storyline)
 
-incredibles = media.Movie("The Incredibles 2",
-                     "The superhero family is back!",
-                     "https://upload.wikimedia.org/wikipedia/en/2/27/The_Incredibles_2.jpg",
-                     "https://www.youtube.com/watch?v=i5qOzqD9Rms")
-#print(avatar.storyline)
+lego_movie = media.Movie(
+    "Lego Movie",
+    "Legos come to life",
+    "https://upload.wikimedia.org/wikipedia/en/1/10/The_Lego_Movie_poster.jpg",
+    "https://www.youtube.com/watch?v=fZ_JOBCLF-I")
 
-spongebob = media.Movie("The SpongeBob SquarePants Movie",
-                     "A Sponge on the Big Screen",
-                     "https://upload.wikimedia.org/wikipedia/en/3/31/The_SpongeBob_SquarePants_Movie_poster.jpg",
-                     "https://www.youtube.com/watch?v=EVhpSZK5KuY")
+incredibles = media.Movie(
+    "The Incredibles 2",
+    "The superhero family is back!",
+    "https://upload.wikimedia.org/wikipedia/en/2/27/The_Incredibles_2.jpg",
+    "https://www.youtube.com/watch?v=i5qOzqD9Rms")
 
-alien = media.Movie("Alien",
-                     "In space, no one can hear you scream",
-                     "https://upload.wikimedia.org/wikipedia/en/c/c3/Alien_movie_poster.jpg",
-                     "https://www.youtube.com/watch?v=LjLamj-b0I8")
+spongebob = media.Movie(
+    "The SpongeBob SquarePants Movie",
+    "A Sponge on the Big Screen",
+    "https://upload.wikimedia.org/wikipedia/en/3/31/"
+    "The_SpongeBob_SquarePants_Movie_poster.jpg",
+    "https://www.youtube.com/watch?v=EVhpSZK5KuY")
 
-neighbor = media.Movie("Wont You Be My Neighbor",
-                     "Documentary on Mr. Rogers",
-                     "https://upload.wikimedia.org/wikipedia/en/7/7d/Won%27t_You_Be_My_Neighbor%3F.png",
-                     "https://www.youtube.com/watch?v=FhwktRDG_aQ")
+alien = media.Movie(
+    "Alien",
+    "In space, no one can hear you scream",
+    "https://upload.wikimedia.org/wikipedia/en/c/c3/Alien_movie_poster.jpg",
+    "https://www.youtube.com/watch?v=LjLamj-b0I8")
 
-panda = media.Movie("Kung Fu Panda",
-                     "The chosen one fights!",
-                     "https://upload.wikimedia.org/wikipedia/en/7/76/Kungfupanda.jpg",
-                     "https://www.youtube.com/watch?v=PXi3Mv6KMzY")
+neighbor = media.Movie(
+    "Wont You Be My Neighbor",
+    "Documentary on Mr. Rogers",
+    "https://upload.wikimedia.org/wikipedia/en/7/7d/"
+    "Won%27t_You_Be_My_Neighbor%3F.png",
+    "https://www.youtube.com/watch?v=FhwktRDG_aQ")
 
-movies = [lego_movie,incredibles,spongebob,alien,neighbor,panda]
+panda = media.Movie(
+    "Kung Fu Panda",
+    "The chosen one fights!",
+    "https://upload.wikimedia.org/wikipedia/en/7/76/Kungfupanda.jpg",
+    "https://www.youtube.com/watch?v=PXi3Mv6KMzY")
+
+movies = [lego_movie, incredibles, spongebob, alien, neighbor, panda]
 fresh_tomatoes.open_movies_page(movies)
--- media.py
+++ media.py
@@ -1,7 +1,9 @@
 import webbrowser
 
+
 class Movie():
-    def __init__(self, movie_title, movie_storyline, poster_image_url, trailer_youtube):
+    def __init__(self, movie_title, movie_storyline,
+                 poster_image_url, trailer_youtube):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image_url
@@ -9,4 +11,3 @@ class Movie():
 
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
-        
