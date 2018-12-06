3572343a70ccfbcd9a857a5a5a076754985cd411
Frank811 <45404050+Frank811@users.noreply.github.com>
2018-12-05 21:27:58 -0600

    Add files via upload

--- entertainment_center.py
+++ entertainment_center.py
@@ -2,43 +2,51 @@ import fresh_tomatoes
 import media
 
 
+# Lego Movie: movie title, storyline, poster image, movie trailer
 lego_movie = media.Movie(
     "Lego Movie",
     "Legos come to life",
     "https://upload.wikimedia.org/wikipedia/en/1/10/The_Lego_Movie_poster.jpg",
     "https://www.youtube.com/watch?v=fZ_JOBCLF-I")
 
+# Incredibles Movie: movie title, storyline, poster image, movie trailer
 incredibles = media.Movie(
     "The Incredibles 2",
     "The superhero family is back!",
     "https://upload.wikimedia.org/wikipedia/en/2/27/The_Incredibles_2.jpg",
     "https://www.youtube.com/watch?v=i5qOzqD9Rms")
 
+# SpongeBob Movie: movie title, storyline, poster image, movie trailer
 spongebob = media.Movie(
     "The SpongeBob SquarePants Movie",
     "A Sponge on the Big Screen",
-    "https://upload.wikimedia.org/wikipedia/en/3/31/"
-    "The_SpongeBob_SquarePants_Movie_poster.jpg",
+    "https://upload.wikimedia.org/wikipedia/en/3/31/The_SpongeBob_SquarePants_Movie_poster.jpg",  # NOQA
     "https://www.youtube.com/watch?v=EVhpSZK5KuY")
 
+# Alien Movie: movie title, storyline, poster image, movie trailer
 alien = media.Movie(
     "Alien",
     "In space, no one can hear you scream",
     "https://upload.wikimedia.org/wikipedia/en/c/c3/Alien_movie_poster.jpg",
     "https://www.youtube.com/watch?v=LjLamj-b0I8")
 
+# Wont You Be My Neighbor Movie:
+# movie title, storyline, poster image, movie trailer
 neighbor = media.Movie(
     "Wont You Be My Neighbor",
     "Documentary on Mr. Rogers",
-    "https://upload.wikimedia.org/wikipedia/en/7/7d/"
-    "Won%27t_You_Be_My_Neighbor%3F.png",
+    "https://upload.wikimedia.org/wikipedia/en/7/7d/Won%27t_You_Be_My_Neighbor%3F.png",  # NOQA
     "https://www.youtube.com/watch?v=FhwktRDG_aQ")
 
+# Kung Fu Panda Movie: movie title, storyline, poster image, movie trailer
 panda = media.Movie(
     "Kung Fu Panda",
     "The chosen one fights!",
     "https://upload.wikimedia.org/wikipedia/en/7/76/Kungfupanda.jpg",
     "https://www.youtube.com/watch?v=PXi3Mv6KMzY")
 
+# list of movies sent to media file
 movies = [lego_movie, incredibles, spongebob, alien, neighbor, panda]
+
+# launches the actual default webbrowser with fresh_tomatoes.py
 fresh_tomatoes.open_movies_page(movies)
--- media.py
+++ media.py
@@ -2,12 +2,24 @@ import webbrowser
 
 
 class Movie():
+    """
+    Movie class that has title, storyline, poster image,
+    and a url to its trailer.
+    
+    """
     def __init__(self, movie_title, movie_storyline,
                  poster_image_url, trailer_youtube):
+        """
+        Constructor takes parameters as strings
+        """
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image_url
         self.trailer_youtube_url = trailer_youtube
 
     def show_trailer(self):
+        """
+        open the trailer_youtube_url link
+        in the default browser
+        """
         webbrowser.open(self.trailer_youtube_url)
